1ec7d8a465494615c18f514c7c24d00ff07a5805
bakas-yannis <bakas.yannis@gmail.com>
2014-04-29 17:21:49 +0300

    Main added on HelloGradle class.

--- src/main/java/com/iccs/gradle/app/HelloGradle.java
+++ src/main/java/com/iccs/gradle/app/HelloGradle.java
@@ -4,4 +4,8 @@ package com.iccs.gradle.app;
  * Created by Giannis on 29/4/2014.
  */
 public class HelloGradle {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Gradle and GitHub...");
+    }
 }
