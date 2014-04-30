3661b2444eb5007147704b6d878923371535015c
bakas-yannis <bakas.yannis@gmail.com>
2014-04-30 18:05:24 +0300

    New bug fix function was added in HelloGradle class

--- src/main/java/com/iccs/gradle/app/HelloGradle.java
+++ src/main/java/com/iccs/gradle/app/HelloGradle.java
@@ -8,4 +8,10 @@ public class HelloGradle {
     public static void main(String[] args) {
         System.out.println("Hello Gradle and GitHub this is a bugFix...");
     }
+
+    public void newBugFixFunc(){
+
+        System.out.println("I do nothing...");
+
+    }
 }
